2aa74fa5889b4c9adc063fe88265439c55c29cf9
RIYA NATH <riyanath138@gmail.com>
2016-10-15 11:26:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/main.js', function (req, res) {
+/*app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js')); //changed to main.js from style.css
 });
+*/
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css')); //changed to main.js from style.css
+});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
